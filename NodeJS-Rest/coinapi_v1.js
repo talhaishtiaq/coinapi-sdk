134c04acee4be778985a19f64827665fabc85e81
Talha Ishtiaq <ti@sourcecrew.io>
2017-05-26 23:32:13 +0500

    NodeJs Api

--- NodeJS-Rest/coinapi_v1.js
+++ NodeJS-Rest/coinapi_v1.js
@@ -48,10 +48,21 @@ method.getSymbols = function(callback) {
     this.HttpClient.PerformReq('https://rest.coinapi.io/v1/symbols', callback);
 };
 //Exchange Rates
-method.GetExchangeRate = function(asset_id_base, asset_id_quote, callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/exchangerate/'+asset_id_base+'/'+asset_id_quote , callback);
+method.GetExchangeRate = function(callback, asset_id_base, asset_id_quote, time = null) {
+	if(asset_id_quote == null){
+		callback("asset_id_quote is required");
+	}if(asset_id_base == null){
+		callback("asset_id_base is required");
+	}if(time != null ){
+		this.HttpClient.PerformReq('https://rest.coinapi.io/v1/exchangerate/'+asset_id_base+'/'+asset_id_quote+'?time='+time , callback);
+	}else{
+		this.HttpClient.PerformReq('https://rest.coinapi.io/v1/exchangerate/'+asset_id_base+'/'+asset_id_quote , callback);
+	}
 };
-method.GetExchangeRates = function(asset_id_base, callback) {
+method.GetExchangeRates = function(callback, asset_id_base) {
+	if(asset_id_base == null){
+		callback("asset_id_base is required");
+	}
     this.HttpClient.PerformReq('https://rest.coinapi.io/v1/exchangerate/' + asset_id_base, callback);
 };
 //OHCLV
@@ -59,48 +70,230 @@ method.GetPeriods = function(callback) {
     this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/periods', callback);
 };
 
-method.GetOHLCVLatest = function(symbol_id, period_id, callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/'+symbol_id + '/latest?period_id=' + period_id, callback);
+method.GetOHLCVLatest = function(callback, symbol_id, period_id, limit = null) {
+	if(symbol_id == null){
+		callback("symbol_id is required");
+	}if(period_id == null){
+		callback("period_id is required");
+	}if(limit != null){
+		this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/'+symbol_id + '/latest?period_id=' + period_id+'&limit='+limit, callback);
+	}else{
+		this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/'+symbol_id + '/latest?period_id=' + period_id, callback);
+	}
 };
-method.GetOHLCVHistory = function(symbol_id, period_id, time_start ,callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/' + symbol_id + '/history?period_id=' + period_id + '&time_start=' + time_start, callback);
+method.GetOHLCVHistory = function(callback, symbol_id, period_id, time_start, time_end = null, limit = null) {
+	if(symbol_id == null){
+		callback("symbol_id is required");
+	}if(period_id == null){
+		callback("period_id is required");
+	}if ($time_start == null){
+        callback("time_start is required");
+    }if ($time_end == null && $limit != null){
+    	this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/' + symbol_id + '/history?period_id=' + period_id + '&time_start=' + time_start + '&limit='+limit, callback);
+    }else if ($limit == null && $time_end != null){
+    	this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/' + symbol_id + '/history?period_id=' + period_id + '&time_start=' + time_start + '&time_end='+time_end, callback);
+	}else if ($limit == null && $time_end == null){
+		this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/' + symbol_id + '/history?period_id=' + period_id + '&time_start=' + time_start, callback);
+    }else{
+    	this.HttpClient.PerformReq('https://rest.coinapi.io/v1/ohlcv/' + symbol_id + '/history?period_id=' + period_id + '&time_start=' + time_start + '&time_end='+time_end+'&limit='+limit, callback);
+    }
 };
 
 //trades
-method.GetTradesLatest = function(callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/trades/latest', callback);
+method.GetTradesLatest = function(callback, symbol_id, limit = null) {
+    if (symbol_id == null && limit == null)
+    {
+        url = 'https://rest.coinapi.io/v1/trades/latest';
+    }
+    else if (symbol_id == null && limit != null)
+    {
+        url = 'https://rest.coinapi.io/v1/trades/latest?limit=' + limit;
+    }
+    else if (limit == null && symbol_id != null)
+    {
+        url = 'https://rest.coinapi.io/v1/trades/' + symbol_id + '/latest';
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/trades/' + symbol_id + '/latest?limit=' + limit;
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
 
-method.GetTradesHistory = function(symbol_id, time_start,  callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/trades/' + symbol_id + '/history?time_start=' + time_start, callback);
+method.GetTradesHistory = function(callback, symbol_id, time_start, time_end = null, limit = null) {
+    if (symbol_id == null)
+    {
+        callback("symbol_id is required");
+    }
+    if (time_start == null)
+    {
+        callback("time_start is required");
+    }
+    if (time_end == null && limit != null)
+    {
+        url= 'https://rest.coinapi.io/v1/trades/' + symbol_id + '/history?time_start='  + time_start + '&limit=' + limit;
+    }
+    else if (limit == null && time_end != null)
+    {
+        url= 'https://rest.coinapi.io/v1/trades/' + symbol_id + '/history?time_start=' + time_start + '&time_end=' + time_end;
+    }
+    else if (limit == null && time_end == null)
+    {
+        url= 'https://rest.coinapi.io/v1/trades/' + symbol_id + '/history?time_start=' + time_start;
+    }
+    else
+    {
+        url= 'https://rest.coinapi.io/v1/trades/' + symbol_id + '/history?time_start=' + time_start + '&time_end=' + time_end + '&limit=' + limit;
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
 //Quotes
-method.GetQuotesCurrent = function(callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/quotes/current', callback);
+method.GetQuotesCurrent = function(callback, symbol_id = null) {
+    if (symbol_id == null)
+    {
+        url = 'https://rest.coinapi.io/v1/quotes/current';
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/quotes/' + symbol_id + '/current';
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
-method.GetQuotesLatest = function(callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/quotes/latest', callback);
+method.GetQuotesLatest = function(callback, symbol_id =null, limit = null) {
+    if (symbol_id == null && limit == null)
+    {
+        url = 'https://rest.coinapi.io/v1/quotes/latest';
+    }
+    else if (symbol_id == null && limit != null)
+    {
+        url = 'https://rest.coinapi.io/v1/quotes/latest?limit=' + limit;
+    }
+    else if ($limit == null && symbol_id != null)
+    {
+        $url = 'https://rest.coinapi.io/v1/quotes/' + symbol_id + '/latest';
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/quotes/' + symbol_id + '/latest?limit=' + limit;
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
 
-method.GetQuotesHistory = function(symbol_id, time_start, callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/quotes/'+ symbol_id + '/history?time_start=' +time_start, callback);
+method.GetQuotesHistory = function(callback, symbol_id, time_start, time_end = null, limit = null) {
+    if (symbol_id == null)
+    {
+        callback("symbol_id is required");
+    }
+    if (time_start == null)
+    {
+        callback("time_start is required");
+    }
+    if (time_end == null && limit != null)
+    {
+        url = 'https://rest.coinapi.io/v1/quotes/' + symbol_id + '/history?time_start=' + time_start + '&limit=' + limit;
+    }
+    else if (limit == null && time_end != null)
+    {
+       url = 'https://rest.coinapi.io/v1/quotes/' + symbol_id + '/history?time_start=' + time_start + '&time_end=' + time_end;
+    }
+    else if (limit == null && time_end == null)
+    {
+    	url = 'https://rest.coinapi.io/v1/quotes/' + symbol_id + '/history?time_start=' + time_start;
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/quotes/' + symbol_id + '/history?time_start=' + time_start + '&time_end=' + time_end + '&limit=' + limit;
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
 //OrderBooks
-method.GetOrderbookCurrent = function(callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/Orderbooks/current', callback);
+method.GetOrderbookCurrent = function(callback, symbol_id = null) {
+    if (symbol_id == null)
+    {
+        url = 'https://rest.coinapi.io/v1/Orderbooks/current';
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/current';
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
-method.GetOrderbookLatest = function(symbol_id, callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/latest',callback);
+method.GetOrderbookLatest = function(callback, symbol_id, limit = null) {
+    if (symbol_id == null)
+    {
+        callback("symbol_id is required");
+    }
+    if (limit == null)
+    {
+        url = 'https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/latest';
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/latest?limit=' + limit;
+    }
+    this.HttpClient.PerformReq(url , callback);
 };
 
-method.GetOrderbookHistory = function(symbol_id, time_start, callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/Orderbooks/'+symbol_id+'/history?time_start='+time_start, callback);
+method.GetOrderbookHistory = function(callback, symbol_id, time_start, time_end = null, limit = null) {
+    if (symbol_id == null)
+    {
+        callback("symbol_id is required");
+    }
+    if (time_start == null)
+    {
+        callback("time_start is required");
+    }
+    if (time_end == null && limit != null)
+    {
+        url = 'https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/history?time_start=' + time_start + '&limit=' + limit;
+    }
+    else if (limit == null && time_end != null)
+    {
+        url= 'https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/history?time_start=' + time_start + '&time_end=' + time_end;
+    }
+    else if (limit == null && time_end == null)
+    {
+        url = 'https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/history?time_start=' + time_start;
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/Orderbooks/' + symbol_id + '/history?time_start=' + time_start + '&time_end=' + time_end + '&limit=' + limit;
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
 //Twitter
-method.GetTwitterLatest = function(callback) {
-    this.HttpClient.PerformReq('https://rest.coinapi.io/v1/twitter/latest', callback);
+method.GetTwitterLatest = function(callback, limit = null) {
+    if (limit == null)
+    {
+        url = 'https://rest.coinapi.io/v1/twitter/latest';
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/twitter/latest?limit=' . limit;
+    }
+    this.HttpClient.PerformReq(url, callback);
 };
-method.GetTwitterHistory = function(time_start, callback) {
+method.GetTwitterHistory = function(callback, time_start, time_end = null, limit = null) {
+    if (time_start == null)
+    {
+        callback("time_start is required");
+    }
+    if (time_end == null && limit != null)
+    {
+        url = 'https://rest.coinapi.io/v1/twitter/history?time_start=' + time_start + '&limit=' + limit;
+    }
+    else if (limit == null && time_end != null)
+    {
+        url = 'https://rest.coinapi.io/v1/twitter/history?time_start=' + time_start + '&time_end=' + time_end;
+    }
+    else if (limit == null && time_end == null)
+    {
+        url = 'https://rest.coinapi.io/v1/twitter/history?time_start=' + time_start;
+    }
+    else
+    {
+        url = 'https://rest.coinapi.io/v1/twitter/history?time_start=' + time_start + '&time_end=' + time_end + '&limit=' + limit;
+    }
     this.HttpClient.PerformReq('https://rest.coinapi.io/v1/twitter/history?time_start='+time_start, callback);
 };
 module.exports = CoinApi;
--- NodeJS-Rest/index.js
+++ NodeJS-Rest/index.js
@@ -2,64 +2,68 @@ var CoinApi = require('./coinapi_v1.js');
 
 
 
-
-var api = new CoinApi('APIKey');
-api.getExchanges(function(Exchanges) {
-	console.log(Exchanges);
-});
-api.getAssets(function(Assets) {
-	console.log(Assets);
-});
-api.getSymbols(function(Symbols) {
-	console.log(Symbols);
-});
-
-api.GetExchangeRate('BTC','USD',function(ExchangeRate) {
-	console.log(ExchangeRate);
-});
-api.GetExchangeRates('BTC',function(ExchangeRates) {
-	console.log(ExchangeRates);
-});
-api.GetPeriods(function(Periods) {
-	console.log(Periods);
-});
-api.GetOHLCVLatest('BITSTAMP_SPOT_BTC_USD','1HRS' , function(OHLCV) {
-	console.log(OHLCV);
-});
 var time_start = new Date();
 time_start.setDate(time_start.getDate()-1);
 time_start = time_start.toISOString();
-api.GetOHLCVHistory('BITSTAMP_SPOT_BTC_USD','1HRS', time_start ,function(OHLCV) {
-	console.log(OHLCV);
-});
-api.GetTradesLatest(function(Trades) {
-	console.log(Trades);
-});
 
-api.GetTradesHistory('BITSTAMP_SPOT_BTC_USD', time_start,  function(Trades) {
-	console.log(Trades);
-});
-api.GetQuotesCurrent(function(Quotes) {
-	console.log(Quotes);
-});
-api.GetQuotesLatest(function(Quotes) {
-	console.log(Quotes);
-});
-api.GetQuotesHistory('BITSTAMP_SPOT_BTC_USD', time_start, function(Quotes) {
-	console.log(Quotes);
-});
-api.GetOrderbookCurrent(function(OrderBooks) {
-	console.log(OrderBooks);
-});
-api.GetOrderbookLatest('BITSTAMP_SPOT_BTC_USD', function(OrderBooks) {
-	console.log(OrderBooks);
-});
-api.GetOrderbookHistory('BITSTAMP_SPOT_BTC_USD', time_start, function(OrderBooks) {
-	console.log(OrderBooks);
-});
-api.GetTwitterLatest(function(Twitter) {
-	console.log(Twitter);
-});
-api.GetTwitterHistory(time_start, function(Twitter) {
-	console.log(Twitter);
-});
+var api = new CoinApi('APIKey');
+// api.getExchanges(function(Exchanges) {
+// 	console.log(Exchanges);
+// });
+// api.getAssets(function(Assets) {
+// 	console.log(Assets);
+// });
+// api.getSymbols(function(Symbols) {
+// 	console.log(Symbols);
+// });
+
+// api.GetExchangeRate(function(ExchangeRate) {
+// 	console.log(ExchangeRate);
+// }, 'BTC','USD');
+// api.GetExchangeRates(function(ExchangeRates) {
+// 	console.log(ExchangeRates);
+// }, 'BTC');
+// api.GetPeriods(function(Periods) {
+// 	console.log(Periods);
+// });
+// api.GetOHLCVLatest(function(OHLCV) {
+// 	console.log(OHLCV);
+// }, 'BITSTAMP_SPOT_BTC_USD','1HRS');
+
+// api.GetOHLCVHistory(function(OHLCV) {
+// 	console.log(OHLCV);
+// }, 'BITSTAMP_SPOT_BTC_USD','1HRS', time_start);
+// api.GetTradesLatest(function(Trades) {
+// 	console.log(Trades);
+// });
+
+// api.GetTradesHistory(function(Trades) {
+// 	console.log(Trades);
+// }, 'BITSTAMP_SPOT_BTC_USD', time_start);
+// api.GetQuotesCurrent(function(Quotes) {
+// 	console.log(Quotes);
+// }, 'BITSTAMP_SPOT_BTC_USD');
+// api.GetQuotesLatest(function(Quotes) {
+// 	console.log(Quotes);
+// });
+// api.GetQuotesHistory(function(Quotes) {
+// 	console.log(Quotes);
+// }, 'BITSTAMP_SPOT_BTC_USD', time_start);
+// api.GetOrderbookCurrent(function(OrderBooks) {
+// 	console.log(OrderBooks);
+// });
+// api.GetOrderbookLatest(function(OrderBooks) {
+// 	console.log(OrderBooks);
+// }, 'BITSTAMP_SPOT_BTC_USD');
+// api.GetOrderbookHistory(function(OrderBooks) {
+// 	console.log(OrderBooks);
+// }, 'BITSTAMP_SPOT_BTC_USD', time_start);
+// api.GetTwitterLatest(function(Twitter) {
+// 	console.log(Twitter);
+// });
+api.GetTwitterHistory(function(Twitter) {
+	Twitter  = JSON.parse(Twitter);
+	Twitter.forEach(function(value, index){
+		console.log(value);
+	});
+}, time_start);
